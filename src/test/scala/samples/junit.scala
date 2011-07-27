38b6d58fcc1847835e83892033c9573d27b1a103
Hizume <hzmyh0@gmail.com>
2011-07-27 17:04:35 +0900

    TDDinScala Commit Test

---
+++ src/test/scala/samples/junit.scala
@@ -0,0 +1,25 @@
+package samples
+
+import org.junit._
+import Assert._
+import com.hzmyh0.tddinscala.Dollar
+
+@Test
+class AppTest {
+
+    @Test
+    def testOK() = assertTrue(true)
+    
+    @Test
+    def testMultiplication() = {
+      val five: Dollar = new Dollar(5)
+      five.times(2)
+      assertEquals(10, five.amount)
+    }
+
+//    @Test
+//    def testKO() = assertTrue(false)
+
+}
+
+
