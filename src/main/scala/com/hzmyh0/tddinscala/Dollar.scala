5671c560ac0c81613562c7cc2b7ac65a2898fb4c
Hizume <hzmyh0@gmail.com>
2011-08-06 08:35:08 +0900

    chap3 end

--- src/main/scala/com/hzmyh0/tddinscala/Dollar.scala
+++ src/main/scala/com/hzmyh0/tddinscala/Dollar.scala
@@ -3,5 +3,10 @@ package com.hzmyh0.tddinscala
 class Dollar (amt: Int) {
   var amount = amt
   def times(multiplier: Int) = {new Dollar(amount * multiplier)}
-  override def equals (other: Any) = {true}
+  override def equals (other: Any) = {
+    other match {
+      case that : Dollar => this.amount == that.amount
+      case _ => false
+    }
+  }
 }
