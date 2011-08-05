ce4de1cadc10b007acc4b5fcba09e730924e232f
Hizume <hzmyh0@gmail.com>
2011-08-06 08:22:14 +0900

    p16 end

--- src/test/scala/samples/junit.scala
+++ src/test/scala/samples/junit.scala
@@ -21,7 +21,8 @@ class AppTest {
     
     @Test
     def testEquality() = {
-      assertTrue(new Dollar(5).equals(new Dollar(5)))
+      assertTrue(new Dollar(5) == new Dollar(5))
+      assertFalse(new Dollar(5) == new Dollar(6))
     }
 
 //    @Test
