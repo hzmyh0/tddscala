feb207c6ed33e27c69ccefead3e1be89d0076eae
Hizume <hzmyh0@gmail.com>
2011-08-06 08:41:51 +0900

    chap4 end

--- src/main/scala/com/hzmyh0/tddinscala/Dollar.scala
+++ src/main/scala/com/hzmyh0/tddinscala/Dollar.scala
@@ -1,7 +1,7 @@
 package com.hzmyh0.tddinscala
 
 class Dollar (amt: Int) {
-  var amount = amt
+  private val amount = amt
   def times(multiplier: Int) = {new Dollar(amount * multiplier)}
   override def equals (other: Any) = {
     other match {
