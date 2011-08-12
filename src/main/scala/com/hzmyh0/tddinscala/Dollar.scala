68c48e2de0ee4178b9304f2c150b7972d57d9ee6
Yohei Hizume <hzmyh0@gmail.com>
2011-08-12 09:46:22 +0900

    p29

--- src/main/scala/com/hzmyh0/tddinscala/Dollar.scala
+++ src/main/scala/com/hzmyh0/tddinscala/Dollar.scala
@@ -4,10 +4,10 @@ package com.hzmyh0.tddinscala
 class Dollar (amt: Int) extends Money {
   amount = amt
   def times(multiplier: Int) = {new Dollar(amount * multiplier)}
-  override def equals (other: Any) = {
-    other match {
-      case that : Dollar => this.amount == that.amount
-      case _ => false
-    }
-  }
+//  override def equals (other: Any) = {
+//    other match {
+//      case that : Money => this.amount == that.amount
+//      case _ => false
+//    }
+//  }
 }
--- src/main/scala/com/hzmyh0/tddinscala/Money.scala
+++ src/main/scala/com/hzmyh0/tddinscala/Money.scala
@@ -1,5 +1,11 @@
 package com.hzmyh0.tddinscala
 
 class Money {
-	protected var amount = 0
+	protected var amount: Int = 0
+	override def equals (other: Any) = {
+    other match {
+      case that : Money => this.amount == that.amount
+      case _ => false
+    }
+  }
 }
