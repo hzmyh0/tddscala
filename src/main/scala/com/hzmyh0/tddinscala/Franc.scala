61db66762427b0f9ecb948fc2e9f545ccd79ed38
Yohei Hizume <hzmyh0@gmail.com>
2011-08-19 09:46:00 +0900

    p46

--- src/main/scala/com/hzmyh0/tddinscala/Franc.scala
+++ src/main/scala/com/hzmyh0/tddinscala/Franc.scala
@@ -1,5 +1,5 @@
 package com.hzmyh0.tddinscala
 
 class Franc (amt: Int, crncy: String) extends Money (amt, crncy) {
-  def times(multiplier: Int) : Money = Money.franc(amount * multiplier)
+  def times(multiplier: Int) : Money = new Franc(amount * multiplier, currency)
 }
