a3e4eda40d289ff0c31c4692f68155b1d962c2f9
Yohei Hizume <hzmyh0@gmail.com>
2011-08-19 09:25:20 +0900

    p40

--- src/main/scala/com/hzmyh0/tddinscala/Franc.scala
+++ src/main/scala/com/hzmyh0/tddinscala/Franc.scala
@@ -1,7 +1,8 @@
 package com.hzmyh0.tddinscala
 
 class Franc (amt: Int) extends Money {
+  private val currencyVal = "CHF"
   amount = amt
   def times(multiplier: Int) : Money = {new Franc(amount * multiplier)}
-  def currency() : String = "CHF"
+  def currency() : String = currencyVal
 }
