4bdf2b0eb5b8d5181f32091027d26cb04ff8c5a3
Yohei Hizume <hzmyh0@gmail.com>
2011-08-19 09:26:12 +0900

    p41

--- src/main/scala/com/hzmyh0/tddinscala/Dollar.scala
+++ src/main/scala/com/hzmyh0/tddinscala/Dollar.scala
@@ -2,7 +2,8 @@ package com.hzmyh0.tddinscala
 //import com.hzmyh0.tddinscala.Money
 
 class Dollar (amt: Int) extends Money {
+  private val currencyVal = "USD"
   amount = amt
   def times(multiplier: Int) : Money = {new Dollar(amount * multiplier)}
-  def currency() : String = "USD"
+  def currency() : String = currencyVal
 }
