59985b0227e1dfa2bab64b5f86ee69e8f1e7b667
Yohei Hizume <hzmyh0@gmail.com>
2011-08-19 09:16:46 +0900

    p38 chap8 end

--- src/main/scala/com/hzmyh0/tddinscala/Money.scala
+++ src/main/scala/com/hzmyh0/tddinscala/Money.scala
@@ -12,5 +12,6 @@ abstract class Money {
 }
 
 object Money {
-  def dollar(amount : Int) : Dollar = new Dollar(amount)
+  def dollar(amount: Int) : Money = new Dollar(amount)
+  def franc(amount: Int) : Money = new Franc(amount)
 }
--- src/test/scala/samples/junit.scala
+++ src/test/scala/samples/junit.scala
@@ -13,8 +13,8 @@ class AppTest {
     @Test
     def testMultiplication() = {
       val five: Money = Money.dollar(5)
-      assertEquals(new Dollar(10), five.times(2))
-      assertEquals(new Dollar(15), five.times(3))
+      assertEquals(Money.dollar(10), five.times(2))
+      assertEquals(Money.dollar(15), five.times(3))
     }
     
     @Test
@@ -26,11 +26,11 @@ class AppTest {
     
     @Test
     def testEquality() = {
-      assertTrue(new Dollar(5) == new Dollar(5))
-      assertFalse(new Dollar(5) == new Dollar(6))
+      assertTrue(Money.dollar(5) == Money.dollar(5))
+      assertFalse(Money.dollar(5) == Money.dollar(6))
       assertTrue(new Franc(5) == new Franc(5))
       assertFalse(new Franc(5) == new Franc(6))
-      assertFalse(new Franc(5) == new Dollar(5))
+      assertFalse(new Franc(5) == Money.dollar(5))
     }
 
 //    @Test
