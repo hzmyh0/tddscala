8f45d92fc1e0c37c23c5ba5840d940b0f988d73e
Yohei Hizume <hzmyh0@gmail.com>
2011-08-19 09:38:04 +0900

    p43

--- src/main/scala/com/hzmyh0/tddinscala/Dollar.scala
+++ src/main/scala/com/hzmyh0/tddinscala/Dollar.scala
@@ -1,9 +1,9 @@
 package com.hzmyh0.tddinscala
 //import com.hzmyh0.tddinscala.Money
 
-class Dollar (amt: Int) extends Money {
-  currencyVal = "USD"
+class Dollar (amt: Int, crncy: String) extends Money {
+  currencyVal = crncy
   amount = amt
-  def times(multiplier: Int) : Money = {new Dollar(amount * multiplier)}
+  def times(multiplier: Int) : Money = Money.dollar(amount * multiplier)
 //  def currency() : String = currencyVal
 }
--- src/main/scala/com/hzmyh0/tddinscala/Money.scala
+++ src/main/scala/com/hzmyh0/tddinscala/Money.scala
@@ -14,6 +14,6 @@ abstract class Money {
 }
 
 object Money {
-  def dollar(amount: Int) : Money = new Dollar(amount)
+  def dollar(amount: Int) : Money = new Dollar(amount, "USD")
   def franc(amount: Int) : Money = new Franc(amount, "CHF")
 }
