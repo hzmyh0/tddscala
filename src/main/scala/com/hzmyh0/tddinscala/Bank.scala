0c962d1df8f41ff1b4ee821fd93f63ef8b7eb581
Yohei Hizume <hzmyh0@gmail.com>
2011-09-02 09:40:29 +0900

    p59

--- src/main/scala/com/hzmyh0/tddinscala/Bank.scala
+++ src/main/scala/com/hzmyh0/tddinscala/Bank.scala
@@ -1,5 +1,5 @@
 package com.hzmyh0.tddinscala
 
 class Bank {
-	def reduce(source: Expression, to: String): Money = {null}
+	def reduce(source: Expression, to: String): Money = {Money.dollar(10)}
 }
