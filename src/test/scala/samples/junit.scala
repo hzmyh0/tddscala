11c1e5d2b9740080f59aa9fbb4338f457c9db2f4
Yohei Hizume <hzmyh0@gmail.com>
2011-09-02 09:28:33 +0900

    p56

--- src/test/scala/samples/junit.scala
+++ src/test/scala/samples/junit.scala
@@ -33,7 +33,8 @@ class AppTest {
     @Test
     def testSimpleAddition() = {
       val sum: Money = Money.dollar(5).plus(Money.dollar(5))
-      assertEquals(Money.dollar(10), sum)
+      val reduced: Money = bank.reduce(sum, "USD")
+      assertEquals(Money.dollar(10), reduced)
     }
 
 }
