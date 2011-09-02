f86eb2a684b399f84c6bee162ee139e3fc43fe89
Yohei Hizume <hzmyh0@gmail.com>
2011-09-02 09:08:32 +0900

    p52

--- src/main/scala/com/hzmyh0/tddinscala/Dollar.scala
+++ src/main/scala/com/hzmyh0/tddinscala/Dollar.scala
@@ -1,6 +1,6 @@
 package com.hzmyh0.tddinscala
 //import com.hzmyh0.tddinscala.Money
 
-class Dollar (amt: Int, crncy: String) extends Money (amt, crncy) {
-//  override def times(multiplier: Int) : Money = new Money(amount * multiplier, currency)
-}
+//class Dollar (amt: Int, crncy: String) extends Money (amt, crncy) {
+////  override def times(multiplier: Int) : Money = new Money(amount * multiplier, currency)
+//}
