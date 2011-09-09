5010455b11d6b98d8f0cda2a3e6c996a394939ec
Yohei Hizume <hzmyh0@gmail.com>
2011-09-09 09:39:32 +0900

    p64

--- src/main/scala/com/hzmyh0/tddinscala/Bank.scala
+++ src/main/scala/com/hzmyh0/tddinscala/Bank.scala
@@ -2,6 +2,7 @@ package com.hzmyh0.tddinscala
 
 class Bank {
 	def reduce(source: Expression, to: String): Money = {
+	  if (source.isInstanceOf[Money]) return source.asInstanceOf[Money]
 	  val sum: Sum = source.asInstanceOf[Sum]
 //	  val amount: Int = sum.augend.amount + sum.addend.amount
 //		return new Money(amount, to)
--- src/test/scala/samples/junit.scala
+++ src/test/scala/samples/junit.scala
@@ -55,6 +55,13 @@ class AppTest {
       val result: Money = bank.reduce(sum, "USD")
       assertEquals(Money.dollar(7), result)
     }
+    
+    @Test
+    def testReduceMoney() = {
+      val bank: Bank = new Bank()
+      val result: Money = bank.reduce(Money.dollar(1), "USD")
+      assertEquals(Money.dollar(1), result)
+    }
 }
 
 
