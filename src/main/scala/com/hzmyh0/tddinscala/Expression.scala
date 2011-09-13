741df53ddcc0f7bdeafea6ddbb3628d104e8cee3
Yohei Hizume <hzmyh0@gmail.com>
2011-09-14 07:47:00 +0900

    p68

--- src/main/scala/com/hzmyh0/tddinscala/Expression.scala
+++ src/main/scala/com/hzmyh0/tddinscala/Expression.scala
@@ -1,5 +1,5 @@
 package com.hzmyh0.tddinscala
 
 trait Expression {
-  def reduce(to: String): Money
+  def reduce(bank: Bank, to: String): Money
 }
--- src/main/scala/com/hzmyh0/tddinscala/Money.scala
+++ src/main/scala/com/hzmyh0/tddinscala/Money.scala
@@ -13,9 +13,10 @@ class Money(amt: Int, crncy: String) extends Expression {
   	}
 	override def toString() : String = amount + " " + currency
 	def plus(addend: Money): Expression = new Sum(this, addend)
-	def reduce(to: String): Money = {
-	  val rate: Int = if (currency.equals("CHF") && to.equals("USD")) 2 else 1
-	  return new Money(amount / rate, to)
+	def reduce(source: Expression, to: String): Money = {
+//	  val rate: Int = if (currency.equals("CHF") && to.equals("USD")) 2 else 1 
+//	  return new Money(amount / rate, to)
+	  return source.reduce(this, to)
 	}
 }
 
