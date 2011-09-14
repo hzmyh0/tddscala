dd73710bc69c88289241167b3bce272f0cb5d97d
Yohei Hizume <hzmyh0@gmail.com>
2011-09-14 10:10:38 +0900

    p70 hashtable -> HashMap

--- src/main/scala/com/hzmyh0/tddinscala/Bank.scala
+++ src/main/scala/com/hzmyh0/tddinscala/Bank.scala
@@ -1,12 +1,17 @@
 package com.hzmyh0.tddinscala
-import scala.collection.mutable.HashMap
+import scala.collection.mutable.Map
 
 class Bank() {
-	var rates: HashMap[Pair, Int] = HashMap[Pair, Int]()
+//	var rates: HashMap[Pair, Int] = HashMap[Pair, Int]()
+	var rates: Map[Pair, Int] = Map[Pair, Int]()
 	def reduce(source: Expression, to: String): Money = {
 			source.reduce(this, to)
 	}
 	def rate(from: String, to: String): Int = {
 	  if (from.equals("CHF") && to.equals("USD")) 2 else 1
+	}
+	def addRate(from: String, to: String, rate: Int): Unit = {
+//	  rates.put(new Pair(from, to), rate)
+	  rates += (new Pair(from ,to) -> rate)
 	}
 }
